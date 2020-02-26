a1f66d133ebc68071d60679c6977c5eae290fcfd
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-02-26 22:00:39 +0100

    Add files via upload

--- Pagina_Web/hotelcamponuevo.php
+++ Pagina_Web/hotelcamponuevo.php
@@ -18,8 +18,8 @@ include 'cabecera.php';
 <!--Empieza numero de plantas del hotel-->
 <div class="card" style="width: 35rem;">
   <div class="card-body">
-
-  <table class="table table-bordered"> 
+<table class="table table-bordered table1">
+ 
   <tbody>
     <tr>
       <td id ="1-1" scope="row" >1</td>
@@ -31,7 +31,7 @@ include 'cabecera.php';
     </tr>
     <tr>
       <td id ="1-2" scope="row">2</td>
-      <td  id ="piscina" colspan="4" rowspan="4"></td>
+      <td  class ="piscina" colspan="4" rowspan="4"></td>
       <td id ="1-12">12</td>
       
     </tr>
@@ -57,10 +57,132 @@ include 'cabecera.php';
     </tr>
   </tbody>
 </table>
-
-  </div>
+<table class="table table-bordered table2">
+ 
+  <tbody>
+    <tr>
+      <td id ="2-1" scope="row" >tabla2</td>
+      <td id ="2-8">7</td>
+      <td id ="2-7">8</td>
+      <td id ="2-9">9</td>
+       <td id ="2-10">10</td>
+      <td id ="2-11">11</td>
+    </tr>
+    <tr>
+      <td id ="2-2" scope="row">2</td>
+      <td  class ="piscina" colspan="4" rowspan="4"></td>
+      <td id ="2-12">12</td>
+      
+    </tr>
+     <tr >
+      <td id="2-3" scope="row">3</td>
+      <td id ="2-13">13</td>
+    </tr>
+      <tr>
+      <td id ="2-4" scope="row">4</td>
+      <td id ="2-14" >14</td>
+    </tr>
+      <tr>
+      <td id ="2-5" scope="row">5</td>
+      <td id ="2-15" >15</td>
+    </tr>
+    <tr>
+      <td id ="2-6" scope="row">6</td>
+      <td id ="2-16" >16</td>
+      <td id ="2-17">17</td>
+      <td id ="2-18">18</td>
+      <td id ="2-19" >19</td>
+      <td id ="2-20">20</td>
+    </tr>
+  </tbody>
+</table>
+<table class="table table-bordered table3">
+ 
+  <tbody>
+    <tr>
+      <td id ="3-1" scope="row" >tabla3</td>
+      <td id ="3-7">7</td>
+      <td id ="3-8">8</td>
+      <td id ="3-9">9</td>
+       <td id ="3-10">10</td>
+      <td id ="3-11">11</td>
+    </tr>
+    <tr>
+      <td id ="3-2" scope="row">2</td>
+      <td  class ="piscina" colspan="4" rowspan="4"></td>
+      <td id ="3-12">12</td>
+      
+    </tr>
+     <tr >
+      <td id="3-3" scope="row">3</td>
+      <td id ="3-14">14</td>
+    </tr>
+      <tr>
+      <td id ="3-4" scope="row">4</td>
+      <td id ="3-13" >14</td>
+    </tr>
+      <tr>
+      <td id ="3-5" scope="row">5</td>
+      <td id ="3-15" >15</td>
+    </tr>
+    <tr>
+      <td id ="3-6" scope="row">6</td>
+      <td id ="3-16" >16</td>
+      <td id ="3-17">17</td>
+      <td id ="3-18">18</td>
+      <td id ="3-19" >19</td>
+      <td id ="3-20">20</td>
+    </tr>
+  </tbody>
+</table>
+<table class="table table-bordered table4">
+ 
+  <tbody>
+    <tr>
+      <td id ="4-1" scope="row" >tabla4</td>
+      <td id ="4-7">7</td>
+      <td id ="4-8">8</td>
+      <td id ="4-10">10</td>
+       <td id ="4-9">9</td>
+      <td id ="4-11">11</td>
+    </tr>
+    <tr>
+      <td id ="4-2" scope="row">2</td>
+      <td  class ="piscina" colspan="4" rowspan="4"></td>
+      <td id ="4-12">12</td>
+      
+    </tr>
+     <tr >
+      <td id="4-3" scope="row">3</td>
+      <td id ="4-13">13</td>
+    </tr>
+      <tr>
+      <td id ="4-4" scope="row">4</td>
+      <td id ="4-14" >14</td>
+    </tr>
+      <tr>
+      <td id ="4-5" scope="row">5</td>
+      <td id ="4-15" >15</td>
+    </tr>
+    <tr>
+      <td id ="4-16" scope="row">16</td>
+      <td id ="4-6" >6</td>
+      <td id ="4-17">17</td>
+      <td id ="4-18">18</td>
+      <td id ="4-19" >19</td>
+      <td id ="4-20">20</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 </div>
 <!--FIN numero de plantas del hotel-->
+
+
+<button id="1">Planta1</button>
+<button id="2">Planta2</button>
+<button id="3">Planta3</button>
+<button id="4">Planta4</button>
 
 
 
--- Pagina_Web/links.php
+++ Pagina_Web/links.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <script src="assets/js/jquery.min.js"></script>
+   
+    <script src="assets/js/holacamponuevo.js"></script>
