b43f5ea109a74d76ffc1e54a9db1e6f119402bf8
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-02-26 21:04:57 +0100

    Add files via upload

---
+++ Pagina_Web/hotelcamponuevo.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php 
+include 'links.php';
+?>
+
+    <title>Document</title>
+</head>
+<body>
+<?php 
+include 'cabecera.php';
+?>
+<div class="container">
+<table class="table table-bordered">
+ 
+  <tbody>
+    <tr>
+      <td id ="1-1" scope="row" >1</td>
+      <td id ="1-7">7</td>
+      <td id ="1-8">8</td>
+      <td id ="1-9">9</td>
+       <td id ="1-10">10</td>
+      <td id ="1-11">11</td>
+    </tr>
+    <tr>
+      <td id ="1-2" scope="row">2</td>
+      <td  id ="piscina" colspan="4" rowspan="4"></td>
+      <td id ="1-12">12</td>
+      
+    </tr>
+     <tr >
+      <td id="1-3" scope="row">3</td>
+      <td id ="1-13">13</td>
+    </tr>
+      <tr>
+      <td id ="1-4" scope="row">4</td>
+      <td id ="1-14" >14</td>
+    </tr>
+      <tr>
+      <td id ="1-5" scope="row">5</td>
+      <td id ="1-15" >15</td>
+    </tr>
+    <tr>
+      <td id ="1-6" scope="row">6</td>
+      <td id ="1-16" >16</td>
+      <td id ="1-17">17</td>
+      <td id ="1-18">18</td>
+      <td id ="1-19" >19</td>
+      <td id ="1-20">20</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+
+<?php 
+include 'pie.php';
+?>
