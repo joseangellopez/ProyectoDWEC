efb87fe5f713148743605c6c9a7b2e86349685b9
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-02-27 10:06:40 +0100

    Add files via upload

---
+++ Pagina_Web/conexionbd.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+    $user = "root";  // usuario con el que se va conectar con MySQL
+    $pass = "";  // contraseña del usuario
+    $dbname = "hotel";  //nombre de la base de datos
+    $db = new PDO("mysql:host=localhost; dbname=$dbname", $user, $pass);  //conectar con MySQL y SELECCIONAR LA BBDD
+    $db->exec("set names utf8");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {  // Si hubieran errores de conexión, se captura un objeto de tipo PDOException
+    print "<p>Error: NO SE PUDO CONECTAR AL SERVIDOR MySQL o a la BASE DE DATOS.</p>\n";
+    print "<p>Error: " . $e->getMessage() . "</p>\n";  // mensaje de excepción
+    exit();
+}
+?>
