7d4fc1a23c66050885c5d07aff80c4a164a48f45
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-03-01 16:33:36 +0100

    Registro funcional que encriptar la contraseña

--- Pagina_Web/cabecera.php
+++ Pagina_Web/cabecera.php
@@ -1,5 +1,4 @@
 
-
   <!--ETIQUETA NAV-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="#">HOTEL</a>
@@ -17,7 +16,62 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
         </li>
+        <li><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModallogin">Login</button></li>
+       <li> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModalregistro">Registro</button></li>'
+        <li> <a href="logout.php"> <button type="button" class="btn btn-primary">Logout</button></a></li>
       </ul>
     </div>
+    
 </nav>
   <!-- FIN ETIQUETA NAV-->
+  
+
+<!-- Ventana modal de login y registro -->
+<div class="modal" id="myModallogin" data-backdrop="static">
+
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">login</h4>
+                <button type="button" class="close" data-dismiss="modal"><i class='fa fa-close'
+                                                                            style='font-size:24px;color:red'></i>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+                <?php include 'login.php'; ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<div class="modal" id="myModalregistro" data-backdrop="static">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Registro</h4>
+                <button type="button" class="close" data-dismiss="modal"><i class='fa fa-close'
+                                                                            style='font-size:24px;color:red'></i>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+                <?php include 'registro.php'; ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
---
+++ Pagina_Web/registro.php
@@ -0,0 +1,58 @@
+ <?php
+    include 'conexionbd.php';
+    $msg = "";
+    if (isset($_POST['enviar_registro'])) {             
+        $usuario_registro = trim($_POST['usuario_registro']);
+        $contrasena_registro = trim($_POST['contrasena_registro']);
+        $contrasena_registro2 = trim($_POST['contrasena_registro2']);
+        if (strlen($contrasena_registro) < 6) {
+            echo "<script>alert(\"La costraseña tiene que tener minimo 6 carapteres\");</script>";
+            //$msg = "La costraseña tiene que tener minimo 6 carapteres";
+        } else if ($contrasena_registro != $contrasena_registro2) {
+             echo "<script>alert(\"Las costraseñas tiene que ser iguales\");</script>";
+            //$msg = "Las costraseñas tiene que ser iguales";
+        } else {
+        $contraseña = password_hash($contrasena_registro, PASSWORD_DEFAULT);
+            $sql = $db->prepare("INSERT INTO login(usuario, clave) VALUES (:usuario_registro,:contrasena_registro);");
+            $sql->bindParam('contrasena_registro', $contraseña, PDO::PARAM_STR);
+            $sql->bindParam('usuario_registro', $usuario_registro, PDO::PARAM_STR);
+            $sql->execute();
+        }
+    }
+
+    ?>
+
+
+
+
+<form method="post" name="registro">
+    <div class="container">
+        <div class="form-row">
+            <div class="col-md-5">
+                <div class="form-group">
+                    <input class="form-control" type="text" id="usuario_registro" name="usuario_registro"
+                           placeholder="Usuario" required>
+                   
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    
+                    <input id="myPassword" class="form-control" type="password" id="contrasena_registro" name="contrasena_registro"
+                           placeholder="Contraseña" required>
+                    <input class="form-control" type="password" id="contrasena_registro2" name="contrasena_registro2"
+                           placeholder="Repite Contraseña" required>
+                    <div class="checkbox">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-11"><p class="boton"><input type="submit" name="enviar_registro" id="enviar_registro"
+                                                   value="Login"/></p></div>
+</form>
+
+<span class="text-danger"><?php echo @$msg ?></span>
+
