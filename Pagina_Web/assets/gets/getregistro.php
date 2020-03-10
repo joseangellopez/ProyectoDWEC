b1908ddf94eccd70048391190c34b7dce2677151
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-03-10 21:10:09 +0100

    Add files via upload

--- Pagina_Web/assets/gets/getregistro.php
+++ Pagina_Web/assets/gets/getregistro.php
@@ -4,11 +4,25 @@
         $usuario_registro = trim($_POST['usuario_registro']);
         $contrasena_registro = trim($_POST['contrasena_registro']);
         $contrasena_registro2 = trim($_POST['contrasena_registro2']);
+       $nombre_registro = trim($_POST['nombre_registro']);
+       $apellidos_registro = trim($_POST['apellidos_registro']);
+       $dni_registro = trim($_POST['dni_registro']);
+       $telefono_registro = trim($_POST['telefono_registro']);
+
         $contrasena = password_hash($contrasena_registro2, PASSWORD_DEFAULT);
             $sql = $db->prepare("INSERT INTO login(usuario, clave) VALUES (:usuario_registro,:contrasena_registro2);");
             $sql->bindParam('usuario_registro', $usuario_registro, PDO::PARAM_STR);
             $sql->bindParam('contrasena_registro2', $contrasena, PDO::PARAM_STR);
           $sql->execute();
+
+
+            $sql2 = $db->prepare("INSERT INTO cliente(DNI,nombre,apellidos,telefono) VALUES (:dni_registro,:nombre_registro,:apellidos_registro,:telefono_registro);");
+            $sql2->bindParam('dni_registro', $dni_registro, PDO::PARAM_STR);
+ $sql2->bindParam('nombre_registro', $nombre_registro, PDO::PARAM_STR);
+            $sql2->bindParam('apellidos_registro', $apellidos_registro, PDO::PARAM_STR);
+            $sql2->bindParam('telefono_registro', $telefono_registro, PDO::PARAM_STR);
+
+          $sql2->execute();
             echo ("OK");
 
         
--- Pagina_Web/assets/js/registro.js
+++ Pagina_Web/assets/js/registro.js
@@ -1,22 +1,21 @@
+
 $('#enviar_registro').click(function () {
     console.log("entra al ajax");
     var usuario_registro = $('#usuario_registro').val();
     var contrasena_registro = $('#contrasena_registro').val();
     var contrasena_registro2 = $('#contrasena_registro2').val();
-    
-    if (contrasena_registro != contrasena_registro2){
-        alert("Las contraseñas tienen que ser iguales");
-    }
+    var dni_registro = $('#dni_registro').val();
+        var nombre_registro = $('#nombre_registro').val();
+    var apellidos_registro = $('#apellidos_registro').val();
+        var telefono_registro = $('#telefono_registro').val();
 
-     if (contrasena_registro.length < 6){
-        alert("Las contraseñas tienen que tener minimo 6 caracteres");
-    }
 
-    $.ajax({
+    if (contrasena_registro == contrasena_registro2 && contrasena_registro.length < 6){
+         $.ajax({
         url: "assets/gets/getregistro.php",
         type: "POST",
         data: {usuario_registro: usuario_registro, 
-        contrasena_registro: contrasena_registro,contrasena_registro2: contrasena_registro2},
+        contrasena_registro: contrasena_registro,contrasena_registro2: contrasena_registro2,dni_registro: dni_registro,nombre_registro: nombre_registro,apellidos_registro: apellidos_registro,telefono_registro: telefono_registro},
         success: function (data){
             alert(data);
             if (data == 'OK') {
@@ -28,5 +27,9 @@ $('#enviar_registro').click(function () {
             }
         }
     });
+    }
+
+   
+   
 });
     
