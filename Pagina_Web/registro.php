760b4ffe0f207490be5958c652daa8614b7d84d9
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-03-10 21:09:41 +0100

    Add files via upload

--- Pagina_Web/registro.php
+++ Pagina_Web/registro.php
@@ -1,20 +1,29 @@
+  <link rel="stylesheet" type="text/css" href="administracion/administracion/librerias/alertifyjs/css/alertify.css">
+  <link rel="stylesheet" type="text/css" href="administracion/administracion/librerias/alertifyjs/css/themes/default.css">
 <form method="post" name="registro">
     <div class="container">
         <div class="form-row">
             <div class="col-md-5">
                 <div class="form-group">
-                    <input class="form-control" type="text" id="usuario_registro" name="usuario_registro"
-                           placeholder="Usuario" required>
-                   
+                    <input class="form-control" type="email" id="usuario_registro" name="usuario_registro"
+                           placeholder="Correo electronico" required>
+                    
+                   <input class="form-control" type="text" id="nombre_registro" name="nombre_registro"
+                           placeholder="Nombre" required>
+                      <input class="form-control" type="text" id="apellidos_registro" name="apellidos_registro"
+                           placeholder="Apellidos" required>
+                     <input class="form-control" type="text" id="telefono_registro" name="telefono_registro"
+                           placeholder="telefono" required>
                 </div>
             </div>
             <div class="col-md-1"></div>
             <div class="col-md-6">
                 <div class="form-group">
+                     <input class="form-control" type="text" id="dni_registro" name="dni_registro"
+                           placeholder="DNI" required>
                     
                     <input class="form-control" type="password" id="contrasena_registro" name="contrasena_registro"
                            placeholder="Contraseña" required>
-                    <br>
                     <input class="form-control" type="password" id="contrasena_registro2" name="contrasena_registro2"
                            placeholder="Repite Contraseña" required>
                     <div class="checkbox">
