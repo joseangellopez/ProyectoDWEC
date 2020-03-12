bd14f621b94e16e8d0be32ace0e8d5b37c0ca655
Sergio Rodríguez Siles <sergio4j@hotmail.com>
2020-03-12 11:30:30 +0100

    Add files via upload

--- Pagina_Web/bd/hotel.sql
+++ Pagina_Web/bd/hotel.sql
@@ -1,32 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 12-03-2020 a las 11:23:18
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.2
+
+CREATE database hotel;
+
+use hotel;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `hotel`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
 
 CREATE TABLE `cliente` (
   `codCliente` int(10) NOT NULL,
@@ -36,21 +17,12 @@ CREATE TABLE `cliente` (
   `telefono` varchar(9) COLLATE utf8_bin NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Volcado de datos para la tabla `cliente`
---
-
 INSERT INTO `cliente` (`codCliente`, `DNI`, `nombre`, `apellidos`, `telefono`) VALUES
 (143, '52364789y', 'Antonia', 'Lopera', '666666666'),
 (83, '78124596K', 'Alberto', 'Perez Martinez', '674125578'),
 (142, '78541235u', 'Sergio', 'Rodríguez ', '685414789'),
 (141, '26851201e', 'Jose anges', 'De los Santos', '621457789');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `habitacion`
---
 
 CREATE TABLE `habitacion` (
   `codHabitacion` int(10) NOT NULL,
@@ -63,18 +35,9 @@ CREATE TABLE `habitacion` (
   `Fecha_salida` date DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Volcado de datos para la tabla `habitacion`
---
 
 INSERT INTO `habitacion` (`codHabitacion`, `tipo`, `capacidad`, `planta`, `tarifa`, `reservada`, `Fecha_entrada`, `Fecha_salida`) VALUES
 (37, 'rr', 5, 1, 20, 'SI', '2020-03-27', '2020-04-04');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `login`
---
 
 CREATE TABLE `login` (
   `usuario` varchar(25) COLLATE utf8_bin NOT NULL,
@@ -83,11 +46,6 @@ CREATE TABLE `login` (
   `codCliente` int(10) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `reserva`
---
 
 CREATE TABLE `reserva` (
   `codHabitacion` int(10) NOT NULL,
@@ -96,54 +54,25 @@ CREATE TABLE `reserva` (
   `fechaSalida` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `cliente`
---
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`codCliente`);
 
---
--- Indices de la tabla `habitacion`
---
 ALTER TABLE `habitacion`
   ADD PRIMARY KEY (`codHabitacion`,`planta`),
   ADD KEY `planta` (`planta`);
 
---
--- Indices de la tabla `login`
---
 ALTER TABLE `login`
   ADD PRIMARY KEY (`usuario`),
   ADD KEY `codCliente` (`codCliente`);
 
---
--- Indices de la tabla `reserva`
---
 ALTER TABLE `reserva`
   ADD PRIMARY KEY (`codHabitacion`,`codCliente`,`fechaEntrada`),
   ADD KEY `codCliente` (`codCliente`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `cliente`
---
 ALTER TABLE `cliente`
   MODIFY `codCliente` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
 
---
--- AUTO_INCREMENT de la tabla `habitacion`
---
 ALTER TABLE `habitacion`
   MODIFY `codHabitacion` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
