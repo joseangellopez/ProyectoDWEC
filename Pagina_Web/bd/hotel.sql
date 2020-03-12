2b4f7e3ae9e051220af809bf0dff4357785acf64
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-03-12 11:49:21 +0100

    Add files via upload

--- Pagina_Web/bd/hotel.sql
+++ Pagina_Web/bd/hotel.sql
@@ -36,8 +36,7 @@ CREATE TABLE `habitacion` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-INSERT INTO `habitacion` (`codHabitacion`, `tipo`, `capacidad`, `planta`, `tarifa`, `reservada`, `Fecha_entrada`, `Fecha_salida`) VALUES
-(37, 'rr', 5, 1, 20, 'SI', '2020-03-27', '2020-04-04');
+
 
 CREATE TABLE `login` (
   `usuario` varchar(25) COLLATE utf8_bin NOT NULL,
@@ -73,6 +72,87 @@ ALTER TABLE `reserva`
 ALTER TABLE `cliente`
   MODIFY `codCliente` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
 
-ALTER TABLE `habitacion`
-  MODIFY `codHabitacion` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+
+INSERT INTO `habitacion` (`codHabitacion`, `tipo`, `capacidad`, `planta`, `tarifa`,`reservada`) VALUES
+(1, 'Individual', 1, 1, 50,'SI'),
+(2, 'Individual', 1, 1, 50,'NO'),
+(3, 'Individual', 1, 1, 50,'SI'),
+(4, 'Individual', 1, 1, 50,'NO'),
+(5, 'Individual', 1, 1, 50,'SI'),
+(6, 'Individual', 1, 1, 50,'SI'),
+(7, 'Individual', 2, 1, 70,'NO'),
+(8, 'Individual', 2, 1, 70,'SI'),
+(9, 'Individual', 2, 1, 70,'SI'),
+(10, 'Individual', 2, 1, 70,'SI'),
+(11, 'Individual', 1, 1, 50,'SI'),
+(12, 'Individual', 1, 1, 50,'NO'),
+(13, 'Individual', 1, 1, 50,'SI'),
+(14, 'Individual', 1, 1, 50,'NO'),
+(15, 'Individual', 1, 1, 50,'SI'),
+(16, 'Individual', 1, 1, 50,'SI'),
+(17, 'Individual', 2, 1, 70,'NO'),
+(18, 'Individual', 2, 1, 70,'SI'),
+(19, 'Individual', 2, 1, 70,'SI'),
+(20, 'Individual', 2, 1, 70,'SI'),
+(1, 'Individual', 2, 2, 100,'NO'),
+(2, 'Individual', 2, 2, 100,'NO'),
+(3, 'Individual', 2, 2, 100,'SI'),
+(4, 'Individual', 2, 2, 100,'SI'),
+(5, 'Doble', 3, 2, 150,'SI'),
+(6, 'Doble', 3, 2, 150,'NO'),
+(7, 'Doble', 3, 2, 150,'NO'),
+(8, 'Doble', 3, 2, 150,'SI'),
+(9, 'Doble', 3, 2, 150,'SI'),
+(10, 'Doble', 4, 2, 180,'SI'),
+(11, 'Individual', 2, 2, 100,'NO'),
+(12, 'Individual', 2, 2, 100,'NO'),
+(13, 'Individual', 2, 2, 100,'SI'),
+(14, 'Individual', 2, 2, 100,'SI'),
+(15, 'Doble', 3, 2, 150,'SI'),
+(16, 'Doble', 3, 2, 150,'NO'),
+(17, 'Doble', 3, 2, 150,'NO'),
+(18, 'Doble', 3, 2, 150,'SI'),
+(19, 'Doble', 3, 2, 150,'SI'),
+(20, 'Doble', 4, 2, 180,'SI'),
+(1, 'Doble', 4, 3, 220,'NO'),
+(2, 'Doble', 4, 3, 220,'NO'),
+(3, 'Doble', 2, 3, 80,'SI'),
+(4, 'Doble', 2, 3, 80,'SI'),
+(5, 'Doble', 2, 3, 80,'SI'),
+(6, 'Test', 1, 3, 10,'NO'),
+(7, 'Doble', 2, 3, 80,'SI'),
+(8, 'Doble', 2, 3, 80,'SI'),
+(9, 'Doble', 2, 3, 80,'SI'),
+(10, 'Test', 1, 3, 10,'SI'),
+(11, 'Doble', 4, 3, 220,'NO'),
+(12, 'Doble', 4, 3, 220,'NO'),
+(13, 'Doble', 2, 3, 80,'SI'),
+(14, 'Doble', 2, 3, 80,'SI'),
+(15, 'Doble', 2, 3, 80,'SI'),
+(16, 'Test', 1, 3, 10,'NO'),
+(17, 'Doble', 2, 3, 80,'SI'),
+(18, 'Doble', 2, 3, 80,'SI'),
+(19, 'Doble', 2, 3, 80,'SI'),
+(20, 'Test', 1, 3, 10,'SI'),
+(1, 'Doble', 4, 4, 220,'NO'),
+(2, 'Doble', 4, 4, 220,'NO'),
+(3, 'Doble', 2, 4, 80,'SI'),
+(4, 'Doble', 2, 4, 80,'SI'),
+(5, 'Doble', 2, 4, 80,'SI'),
+(6, 'Test', 1, 4, 10,'NO'),
+(7, 'Doble', 2, 4, 80,'SI'),
+(8, 'Doble', 2, 4, 80,'SI'),
+(9, 'Doble', 2, 4, 80,'SI'),
+(10, 'Test', 1, 4, 10,'SI'),
+(11, 'Doble', 4, 4, 220,'NO'),
+(12, 'Doble', 4, 4, 220,'NO'),
+(13, 'Doble', 2, 4, 80,'SI'),
+(14, 'Doble', 2, 4, 80,'SI'),
+(15, 'Doble', 2, 4, 80,'SI'),
+(16, 'Test', 1, 4, 10,'NO'),
+(17, 'Doble', 2, 4, 80,'SI'),
+(18, 'Doble', 2, 4, 80,'SI'),
+(19, 'Doble', 2, 4, 80,'SI'),
+(20, 'Test', 1, 4, 10,'SI');
 COMMIT;
